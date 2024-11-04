1354b8efc371b0d0e2dd75c6495dc09636e37276
Joaquin Basas <u202310688@upc.edu.pe>
2024-11-04 16:19:53 -0500

    cambios ts

--- src/app/pages/customer/user-config/user-config.component.ts
+++ src/app/pages/customer/user-config/user-config.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-config',
@@ -8,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrl: './user-config.component.css'
 })
 export class UserConfigComponent {
+  passwordForm: FormGroup;
+  showPassword = false;
 
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = this.fb.group({
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
+
+  get isPasswordSecure(): boolean {
+    const password = this.passwordForm.get('newPassword')?.value;
+    return password && password.length >= 8;
+  }
+
+  get passwordMismatch(): boolean | undefined{
+    const newPassword = this.passwordForm.get('newPassword')?.value;
+    const confirmPassword = this.passwordForm.get('confirmPassword')?.value;
+    return newPassword !== confirmPassword && this.passwordForm.get('confirmPassword')?.touched;
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  onChangePassword() {
+    if (this.passwordForm.valid && !this.passwordMismatch) {
+      console.log('Changing password...');
+    }
+  }
+
+  onEnable2FA() {
+    console.log('Enabling 2FA...');
+  }
 }
