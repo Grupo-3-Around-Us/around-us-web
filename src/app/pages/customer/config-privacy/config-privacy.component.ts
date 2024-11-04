6f2574725ad694ab68e50f36a63dad9d19cc8819
Joaquin Basas <u202310688@upc.edu.pe>
2024-11-04 17:52:04 -0500

    feature: config-privacy

--- src/app/pages/customer/config-privacy/config-privacy.component.ts
+++ src/app/pages/customer/config-privacy/config-privacy.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavbarConfigComponent } from '../../../shared/components/navbar-config/navbar-config.component';
 @Component({
   selector: 'app-config-privacy',
   standalone: true,
-  imports: [],
+  imports: [NavbarConfigComponent],
   templateUrl: './config-privacy.component.html',
   styleUrl: './config-privacy.component.css'
 })
