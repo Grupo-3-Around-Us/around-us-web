01391d9ccb4544bbea5612ce583d4095830b47af
JoanixX <alvaradocjosorio@gmail.com>
2024-11-05 16:12:57 -0500

    feature: finalización de customer-home

--- src/app/pages/customer/customer-layout/customer-layout.component.ts
+++ src/app/pages/customer/customer-layout/customer-layout.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { InfoPersonalComponent } from '../info-personal/info-personal.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarMiCuentaComponent } from '../../../shared/components/navbar-mi-cuenta/navbar-mi-cuenta.component';
+import { NavbarCustomerComponent } from '../../../shared/components/navbar-customer/navbar-customer.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarMiCuentaComponent],
+  imports: [RouterOutlet, NavbarMiCuentaComponent, NavbarCustomerComponent, CommonModule, FooterComponent],
   templateUrl: './customer-layout.component.html',
   styleUrl: './customer-layout.component.css'
 })
