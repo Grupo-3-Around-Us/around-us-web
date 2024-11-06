12b42c8fc28c4065999cd2e9e11abe72d99e8a1e
LuisIsaacLazaro <luislazaro96m@hotmail.com>
2024-11-05 23:54:15 -0500

    login/form implementado

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule,
+    RouterLink,
+  ],
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+ loginForm: FormGroup;
+ private fb=inject(FormBuilder);
+ private snackBar = inject(MatSnackBar);
+
+ constructor(){
+  this.loginForm=this.fb.group({
+    email:['',[Validators.required, Validators.email]],
+    password:['',[Validators.required, Validators.minLength(6)]],
+  });
+ }
+  controlHasError(control:string, error:string){
+    return this.loginForm.controls[control]
+  }
+
+  private showSnackBar(message:string){
+    this.snackBar.open(message, 'Cerrar',{
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+  onSubmit(){
+    if(this.loginForm.valid){
+      const credentials = this.loginForm.value;
+      console.log('Credenciales:', credentials);
+      this.showSnackBar('Inicio de sesion exitoso');
+    }
+  }
 }
