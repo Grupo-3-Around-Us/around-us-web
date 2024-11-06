70ea3d3378cafc1689b4d9f0740eee6c2ef46a6b
JoanixX <alvaradocjosorio@gmail.com>
2024-11-06 12:34:09 -0500

    feature: avance del event-detail

--- src/app/pages/customer/customer-home/customer-home.component.ts
+++ src/app/pages/customer/customer-home/customer-home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router'; // Importa RouterModule
+import { Event } from '../../../shared/models/event.model'; // Usamos 'type' para la importación de la interfaz
+import { EventService } from '../../../core/services/event.service';
+
 
 @Component({
   selector: 'app-customer-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule], // Agrega RouterModule aquí
   templateUrl: './customer-home.component.html',
-  styleUrl: './customer-home.component.css'
+  styleUrls:['./customer-home.component.css']
 })
 export class CustomerHomeComponent {
+  events: Event[] = [];
 
+  constructor(private eventService: EventService, public router: Router) {}
+
+  ngOnInit() {
+    // Aquí debes cargar los eventos desde el servicio
+    this.events = this.eventService.getEvents(); // Suponiendo que este método te devuelve los eventos
+  }
+  goToEventDetail(eventId: number): void {
+    this.router.navigate(['event-details', eventId]);
+  }
 }
